b2e8ed4ce8b9a47156ca99871b9f997bda40976e
Hackbright Student <info@hackbrightacademy.com>
2016-08-02 17:49:24 -0700

    almost to login

--- server.py
+++ server.py
@@ -4,6 +4,8 @@ from jinja2 import StrictUndefined
 
 from flask import (Flask, render_template, redirect, request, flash, session)
 from flask_debugtoolbar import DebugToolbarExtension
+from flask.ext.login import LoginManager, UserMixin, login_required, login_user, logout_user
+# may not need UserMixin
 
 from model import User, Rating, Movie, connect_to_db, db
 
@@ -12,6 +14,9 @@ app = Flask(__name__)
 
 # Required to use Flask sessions and the debug toolbar
 app.secret_key = "ABC"
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = "login"
 
 # Normally, if you use an undefined variable in Jinja2, it fails
 # silently. This is horrible. Fix this so that, instead, it raises an
@@ -37,17 +42,60 @@ def add_new_user():
 
     # get value from register.html form for username
     username = request.form.get("username")
-    password = request.form.get("password")  # need to fix 
-    print username, password
-    email_list_of_users = User.query.filter_by(email=username).all()
-    print email_list_of_users
-    # if username not in email_list_of_users:
+    password = request.form.get("password") 
+    print username
+    email_list_of_users = User.query.filter_by(email=username).first()
+    print email_list_of_users.email
+
+    if username != email_list_of_users.email:
+        print "True"
+        new_user = User(email=username, password=password)
+        db.session.add(new_user)
+        db.session.commit()
+        return redirect('/')
+    
+    else:
+        print "WHOOPS******************"
+        flash('Your email address is already in use.')
+        return redirect('/register')
 
     # query db for username attr
     # if not in db, add to db
 
     # if in db, redirect to /login 
+@app.route('/login')
+def login_screen():
+    """Go to login page."""
 
+    return render_template("/login.html")
+
+
+@app.route("/login", methods=["POST"])
+def login():
+    
+    username = request.form.get('username')
+    password = request.form.get('password')        
+
+    # query db for username
+    user = User.query.filter_by(email=username).first()
+    print user.password
+    # if username is not db, redirect w/ flashed message
+    if password == user.password:
+        return redirect('/homepage.html')
+    # if username in db,
+        # check if pw == user.password
+            # if yes, then go to home page w/ flashed message, logged in
+            # if not redirect to login page that password incorrect
+    # if password == username + "_secret":
+    #     id = username.split('user')[1]
+    #     user = User(id)
+    #     login_user(user)
+    #     return redirect(request.args.get("next"))
+    # else:
+    #     return abort(401)
+    else:
+        pass
+        # return something   
 
 
 @app.route('/users')
